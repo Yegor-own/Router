1a1119ec64529b77dc2466f2da9d07cb795b9a57
Yegor-own <egor891712@gmail.com>
2022-07-30 22:16:17 +0500

    add  rabbitmq driver

--- switcher/main.go
+++ switcher/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"switcher/rabbit"
+)
+
+func main() {
+	go rabbit.ConnectRabbitMQ()
+}
--- switcher/rabbit/rabbit.go
+++ switcher/rabbit/rabbit.go
@@ -1,1 +1,45 @@
 package rabbit
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func ConnectRabbitMQ() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
